fd798f66d2b668bd22af48a1dfe593f3b48b00f4
Steven Jimenez <stevenmjimenez@gmail.com>
2025-06-30 11:10:08 -0400

    Add ability to convert from f64

--- src/checked_f64.rs
+++ src/checked_f64.rs
@@ -1,8 +1,22 @@
 use crate::Error;
 
+/// Represents a checked floating-point number that ensures it is neither NaN nor infinite.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct CheckedF64(f64);
 
+/// Implementing the ability to convert `CheckedF64` to `f64` safely.
+/// 
+/// This conversion will return an error if the value is NaN or infinite.
+/// 
+/// ## Example
+/// 
+/// ```rust
+/// use checked_float::{CheckedF64, Error};
+/// 
+/// assert_eq!(f64::try_from(CheckedF64::from(42.0)), Ok(42.0));
+/// assert_eq!(f64::try_from(CheckedF64::from(f64::NAN)), Err(Error::NanValue));
+/// assert_eq!(f64::try_from(CheckedF64::from(f64::INFINITY)), Err(Error::InfiniteValue));
+/// ```
 impl TryFrom<CheckedF64> for f64 {
     type Error = Error;
 
@@ -15,23 +29,22 @@ impl TryFrom<CheckedF64> for f64 {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_checked_f64_infinite() {
-        let inf_value = CheckedF64(f64::INFINITY);
-        let result: Result<f64, Error> = inf_value.try_into();
-        assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), Error::InfiniteValue);
-    }
-
-    #[test]
-    fn test_checked_f64_nan() {
-        let nan_value = CheckedF64(f64::NAN);
-        let result: Result<f64, Error> = nan_value.try_into();
-        assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), Error::NanValue);
+/// Implementing the ability to convert `f64` to `CheckedF64`.
+/// 
+/// This conversion will create a `CheckedF64` instance, but it does not check for NaN or
+/// infinite values.
+/// 
+/// ## Example
+/// 
+/// ```rust
+/// use checked_float::CheckedF64;
+/// 
+/// let checked_value = CheckedF64::from(42.0);
+/// 
+/// assert_eq!(f64::try_from(checked_value).unwrap(), 42.0);
+/// ```
+impl From<f64> for CheckedF64 {
+    fn from(value: f64) -> Self {
+        CheckedF64(value)
     }
 }
